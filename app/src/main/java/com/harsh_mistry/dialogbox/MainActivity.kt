26b62d6f9cb9cb751ad261512f57350f18df95b0
harsh317 <mistryhk317@gmail.com>
2022-12-29 12:42:48 +0530

    Date pcker dialog added

--- app/src/main/java/com/harsh_mistry/dialogbox/MainActivity.kt
+++ app/src/main/java/com/harsh_mistry/dialogbox/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.harsh_mistry.dialogbox
 
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,18 @@ class MainActivity : AppCompatActivity() {
 
             }
             builder.show()
+        }
+        val c = Calendar.getInstance()
+        val year =  c.get(Calendar.YEAR)
+        val month =  c.get(Calendar.MONTH)
+        val day =  c.get(Calendar.DAY_OF_MONTH)
+
+        btn_date.setOnClickListener {
+
+            val dpd = DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                txt_date.setText(""+mDay+"/"+mMonth+"/"+mYear)
+            },year,month,day)
+            dpd.show()
         }
 
     }
